daef38419f7977b8a7685fcbc97e2bb706796ee9
Nahom Diro <nahomdiroj@gmail.com>
2024-07-04 14:51:35 +0300

    first commit

---
+++ src/Api/axiosConfig.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+const axiosBase = axios.create({
+    //baseURL: 'http://localhost:5500/api',
+    baseURL: 'https://evangadi-backend-t0yq.onrender.com/api'
+})
+
+export default axiosBase
---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
+import { useEffect, useState, createContext } from "react";
+import axios from "./Api/axiosConfig";
+import Askquestion from "./pages/AskquestionsPage/Askquestion";
+import QuestionDetail from "./pages/QuestiondescPage/QuestionDetail";
+import LayOut from "./Components/Layout/Layout";
+import Auth from "./pages/Auth/Auth";
+
+export const AppState = createContext();
+
+function App() {
+  const [user, setuser] = useState({});
+
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  async function checkUser() {
+    try {
+      const { data } = await axios.get("users/check", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      setuser(data);
+    } catch (error) {
+      console.log(error.response);
+      navigate("/");
+    }
+  }
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+  return (
+    <AppState.Provider value={{ user, setuser }}>
+      <LayOut>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/askquestions" element={<Askquestion />} />
+          <Route
+            path="/answerquestions/:questionid"
+            element={<QuestionDetail />}
+          />
+        </Routes>
+      </LayOut>
+    </AppState.Provider>
+  );
+}
+
+export default App;
